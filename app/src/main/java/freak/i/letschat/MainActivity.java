385016163037610ec573b922c20fb06e6376829f
Shriharsha M <shriharsha05@gmail.com>
2018-08-28 23:29:13 +0530

    Initial commit

---
+++ app/src/main/java/freak/i/letschat/MainActivity.java
@@ -0,0 +1,13 @@
+package freak.i.letschat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
